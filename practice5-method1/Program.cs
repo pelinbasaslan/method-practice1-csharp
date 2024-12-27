a582dfc3484769fedfb869f31ae2b8804b5c6cd9
pelinbasaslan <pelin.basaslan@gmail.com>
2024-12-27 14:27:03 +0300

    static eklendi

--- practice5-method1/Program.cs
+++ practice5-method1/Program.cs
@@ -1,46 +1,50 @@
-﻿//1.Geriye Değer Döndürmeyen Bir void metot. Ekrana sevdiğiniz bir şarkı sözünü yazdırsın. 
+﻿using System;
 
-void WriteTheLyric()
+class Program
 {
-    Console.WriteLine("I'm not living, I'm just killing time");
+   static void Main(string[] args)
+   {
+        //1.Geriye Değer Döndürmeyen Bir void metot. Ekrana sevdiğiniz bir şarkı sözünü yazdırsın. 
+        WriteTheLyric();
+        //2.Geriye Tamsayı Döndüren Bir metot. Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalanı geriye döndürsün.
+        int kalan = RandomSayiMod2Kalan();
+        Console.WriteLine(kalan);
+        //3.Parametre Alan ve Geriye Değer Döndüren Bir Metot.Parametre olarak aldığı iki sayının çarpımını geriye dönsün.
+        int a = 4;
+        int b = 7;
+        Carpim(a, b);
+        Console.WriteLine(Carpim(a,b));
+        //4.Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
+        Greetings("Pelin","Başaslan");
+   }
+
+   //1.Geriye Değer Döndürmeyen Bir void metot. Ekrana sevdiğiniz bir şarkı sözünü yazdırsın.
+   static void WriteTheLyric()
+   {
+        Console.WriteLine("I'm not living, I'm just killing time");
+   }
+
+   //2.Geriye Tamsayı Döndüren Bir metot. Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalanı geriye döndürsün.
+
+   static int RandomSayiMod2Kalan()
+   {
+        Random rnd = new Random();
+        int randomNum = rnd.Next(1, 100);
+        System.Console.WriteLine($"Bilgisayarın rastgele ürettiği sayı: {randomNum}");
+        return randomNum % 2;
+   }
+
+    //3.Parametre Alan ve Geriye Değer Döndüren Bir Metot.Parametre olarak aldığı iki sayının çarpımını geriye dönsün.
+    static int Carpim(int a, int b)
+    {
+        return a * b;
+    }
+
+    //4.Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
+    static void Greetings(string name, string surName)
+    {
+        Console.WriteLine($"Hoşgeldiniz, {name} {surName}!");
+    }
+
+
 }
-
-Console.WriteLine("Sevdiğim bir şarkı sözü.");
-WriteTheLyric();
-
-
-//2.Geriye Tamsayı Döndüren Bir metot. Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalanı geriye döndürsün.
-
-int RandomSayiMod2Kalan()
-{
-    Random rnd = new Random();
-    int randomNum = rnd.Next(1, 100);
-    Console.WriteLine($"random sayı: {randomNum} ");
-    return randomNum % 2;
-}
-int kalan = RandomSayiMod2Kalan();
-Console.WriteLine($"Rastgele bir sayı üretip bu sayının 2'ye bölümünden kalan {kalan}.");
-
-//3.Parametre Alan ve Geriye Değer Döndüren Bir Metot.Parametre olarak aldığı iki sayının çarpımını geriye dönsün.
-
-int Carpim(int a, int b)
-{
-    return a * b;
-}
-int a = 4;
-int b = 7;
-int sonuc = Carpim(a, b);
-Console.WriteLine($"Parametre olarak alınan {a} sayısı ile {b} sayısının çarpımı: {sonuc}");
-
-//4.Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
-//Parametre olarak isim ve soyisim bilgilerini alıp " Hoş Geldiniz {isim} {soyisim} " şeklinde ekrana bassın.
-
-void Greetings(string name, string surName)
-{
-    Console.WriteLine($"Hoşgeldiniz, {name} {surName}!");
-}
-
-Greetings("Pelin", "Başaslan");
-
-
-
